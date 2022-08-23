569275c09ba992b67a068612638afc27a9ddd351
justinchore <jchoustin91@gmail.com>
2022-08-23 14:02:41 -0700

    Finished almost

--- Analysis_PT2/main.py
+++ Analysis_PT2/main.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import os
 # from itertools import count
 # from sqlite3 import Timestamp
 from pyspark.sql import SparkSession
@@ -15,12 +16,13 @@ spark.sparkContext.setLogLevel("WARN")
 
 #Read file with inferred Schema:
 #Comment out others and run your csv file initialization here:
+#Current
 
 #JUSTINS SPARK.READ:
 orders_df = spark.read.format("csv")\
     .option("header", "true")\
     .option("inferSchema", "true")\
-    .load("file:/home/jcho/project_2/p2_Team2_Data.csv")
+    .load("file:/home/jcho/project_2/p2_Team2_Data.csv") #CHANGE!
 
 #JORDANS SPARK.READ:
 
@@ -99,26 +101,33 @@ date_formatted_orders_rdd.cache()
 orders_df_w_date = spark.createDataFrame(date_formatted_orders_rdd, orders_df.schema)
 #Apply filters (feel free to add more here)
 clean_DF = orders_df_w_date.filter(txn_success_filter)\
-    .filter(col("payment_txn_id")\
-    .rlike(txn_id_regex))\
+    .withColumnRenamed("country(state)", "state")\
+    .filter(col("payment_txn_id").rlike(txn_id_regex))\
     .filter(col("country(state)").isin(valid_states))\
     .filter(col("product_category").isin(valid_categories))\
     .cache()       
-clean_DF.show()
+# clean_DF.show()
+#BASE TEMP VIEW CREATE
+clean_DF.createOrReplaceTempView('data')
+
 
 #############START QUERIES HERE################## cd into Analysis_PT2 to run!
 
+cwd = os.getcwd()
+print(cwd)
 ####JORDAN######
-def q1(df):
-    df.createOrReplaceTempView("data")
-    spark.sql("SELECT SUM(Quantity) as numSold, Country, ProductCategory FROM data GROUP BY Country, ProductCategory").toDF("numSold", "Country", "ProductCategory").createOrReplaceTempView("temp")
+print("Jordan's")
+def q1():
+    spark.sql("SELECT SUM(quantity) as numSold, state, product_category FROM data GROUP BY state, product_category").toDF("numSold", "Country", "ProductCategory").createOrReplaceTempView("temp")
     spark.sql("SELECT MAX(numSold), Country FROM temp GROUP BY Country").toDF("numSold", "Country").createOrReplaceTempView("temp2")
-    print(spark.sql("SELECT temp2.Country, temp.ProductCategory, temp2.numSold FROM temp2 LEFT JOIN temp ON temp2.numSold=temp.numSold ORDER BY numSold").show(500))
+    print(spark.sql("SELECT temp2.Country as state, temp.ProductCategory, temp2.numSold FROM temp2 LEFT JOIN temp ON temp2.numSold=temp.numSold ORDER BY numSold").show(5))
+    
+q1()
     
     
 #ALL QUERIES FOR ANDREW MVP QUESTIONS + EXTRA###
-clean_DF.createOrReplaceTempView("Data")
-clean_DF2 = spark.sql("SELECT datetime,product_category,product_name,quantity,state FROM Data")
+print("Andrew's")
+clean_DF2 = spark.sql("SELECT datetime,product_category,product_name,quantity,state FROM data")
 #df2 is our split between date and time, we select date at index [0]
 clean_DF3 = clean_DF2.withColumn('date', split("datetime", " ")[0])
 clean_DF3.createOrReplaceTempView("date_category")
@@ -126,24 +135,24 @@ clean_DF4= spark.sql("SELECT date,product_category,quantity,state,product_name F
 clean_DF4_transformed = clean_DF4.withColumn('new_date',F.to_date(F.unix_timestamp('date', 'MM/dd/yyyy').cast('timestamp')))
 clean_DF4_transformed.createOrReplaceTempView("date_category_count")
 #This data lists the top sales by year by category
-q1DF=spark.sql("SELECT state,product_category AS Brand, product_name AS Model,COUNT(product_name) AS Orders,SUM(quantity) AS TotalProductsSold FROM date_category_count WHERE new_date BETWEEN '2021-01-01' AND '2021-03-31' AND state=='West Virginia' GROUP BY state,product_category,product_name ORDER BY SUM(quantity) DESC LIMIT 5").show()
+q1DF=spark.sql("SELECT state,product_category AS Brand, product_name AS Model,COUNT(product_name) AS Orders,SUM(quantity) AS TotalProductsSold FROM date_category_count WHERE new_date BETWEEN '2021-01-01' AND '2021-03-31' AND state=='West Virginia' GROUP BY state,product_category,product_name ORDER BY SUM(quantity) DESC LIMIT 5").show(5)
 
 #q1DF=spark.sql("SELECT product_category AS Brand,COUNT(product_category) AS BrandOrders,SUM(quantity) AS TotalProducstSold FROM date_category_count WHERE new_date BETWEEN '2021-01-01' AND '2021-12-31' GROUP BY product_category ORDER BY SUM(quantity) DESC")
 #This data lists the most popular product of the fiscal year 2021
 #q1DF=spark.sql("SELECT product_category AS Brand,product_name AS Model,COUNT(product_name) AS Orders,SUM(quantity) AS TotalProductsSold FROM date_category_count WHERE new_date BETWEEN '2021-01-01' AND '2021-12-31' GROUP BY product_category,product_name ORDER BY SUM(quantity) DESC")
 #Overall
-q1DF=spark.sql("SELECT product_category AS Brand, product_name AS Model,COUNT(product_name) AS Orders,SUM(quantity) AS TotalProductsSold FROM date_category_count WHERE new_date BETWEEN '2021-01-01' AND '2021-03-31' GROUP BY product_category,product_name ORDER BY SUM(quantity) DESC LIMIT 5").show()
-q2DF=spark.sql("SELECT product_category AS Brand, product_name AS Model,COUNT(product_name) AS Orders,SUM(quantity) AS TotalProductsSold FROM date_category_count WHERE new_date BETWEEN '2021-04-01' AND '2021-06-30' GROUP BY product_category,product_name ORDER BY SUM(quantity) DESC LIMIT 5").show()
-q3DF=spark.sql("SELECT product_category AS Brand, product_name AS Model,COUNT(product_name) AS Orders,SUM(quantity) AS TotalProductsSold FROM date_category_count WHERE new_date BETWEEN '2021-07-01' AND '2021-09-30' GROUP BY product_category,product_name ORDER BY SUM(quantity) DESC LIMIT 5").show()
-q4DF=spark.sql("SELECT product_category AS Brand, product_name AS Model,COUNT(product_name) AS Orders,SUM(quantity) AS TotalProductsSold FROM date_category_count WHERE new_date BETWEEN '2021-10-01' AND '2021-12-31' GROUP BY product_category,product_name ORDER BY SUM(quantity) DESC LIMIT 5").show()
+q1DF=spark.sql("SELECT product_category AS Brand, product_name AS Model,COUNT(product_name) AS Orders,SUM(quantity) AS TotalProductsSold FROM date_category_count WHERE new_date BETWEEN '2021-01-01' AND '2021-03-31' GROUP BY product_category,product_name ORDER BY SUM(quantity) DESC LIMIT 5").show(5)
+q2DF=spark.sql("SELECT product_category AS Brand, product_name AS Model,COUNT(product_name) AS Orders,SUM(quantity) AS TotalProductsSold FROM date_category_count WHERE new_date BETWEEN '2021-04-01' AND '2021-06-30' GROUP BY product_category,product_name ORDER BY SUM(quantity) DESC LIMIT 5").show(5)
+q3DF=spark.sql("SELECT product_category AS Brand, product_name AS Model,COUNT(product_name) AS Orders,SUM(quantity) AS TotalProductsSold FROM date_category_count WHERE new_date BETWEEN '2021-07-01' AND '2021-09-30' GROUP BY product_category,product_name ORDER BY SUM(quantity) DESC LIMIT 5").show(5)
+q4DF=spark.sql("SELECT product_category AS Brand, product_name AS Model,COUNT(product_name) AS Orders,SUM(quantity) AS TotalProductsSold FROM date_category_count WHERE new_date BETWEEN '2021-10-01' AND '2021-12-31' GROUP BY product_category,product_name ORDER BY SUM(quantity) DESC LIMIT 5").show(5)
 #By State
-q1DFS=spark.sql("SELECT state,product_category AS Brand, product_name AS Model,COUNT(product_name) AS Orders,SUM(quantity) AS TotalProductsSold FROM date_category_count WHERE new_date BETWEEN '2021-01-01' AND '2021-03-31' GROUP BY state,product_category,product_name ORDER BY SUM(quantity) DESC LIMIT 5").show()
-q2DFS=spark.sql("SELECT state,product_category AS Brand, product_name AS Model,COUNT(product_name) AS Orders,SUM(quantity) AS TotalProductsSold FROM date_category_count WHERE new_date BETWEEN '2021-04-01' AND '2021-06-30' GROUP BY state,product_category,product_name ORDER BY SUM(quantity) DESC LIMIT 5").show()
-q3DFS=spark.sql("SELECT state,product_category AS Brand, product_name AS Model,COUNT(product_name) AS Orders,SUM(quantity) AS TotalProductsSold FROM date_category_count WHERE new_date BETWEEN '2021-07-01' AND '2021-09-30' GROUP BY state,product_category,product_name ORDER BY SUM(quantity) DESC LIMIT 5").show()
-q4DFS=spark.sql("SELECT state,product_category AS Brand, product_name AS Model,COUNT(product_name) AS Orders,SUM(quantity) AS TotalProductsSold FROM date_category_count WHERE new_date BETWEEN '2021-10-01' AND '2021-12-31' GROUP BY state,product_category,product_name ORDER BY SUM(quantity) DESC LIMIT 5").show()
+q1DFS=spark.sql("SELECT state,product_category AS Brand, product_name AS Model,COUNT(product_name) AS Orders,SUM(quantity) AS TotalProductsSold FROM date_category_count WHERE new_date BETWEEN '2021-01-01' AND '2021-03-31' GROUP BY state,product_category,product_name ORDER BY SUM(quantity) DESC LIMIT 5").show(5)
+q2DFS=spark.sql("SELECT state,product_category AS Brand, product_name AS Model,COUNT(product_name) AS Orders,SUM(quantity) AS TotalProductsSold FROM date_category_count WHERE new_date BETWEEN '2021-04-01' AND '2021-06-30' GROUP BY state,product_category,product_name ORDER BY SUM(quantity) DESC LIMIT 5").show(5)
+q3DFS=spark.sql("SELECT state,product_category AS Brand, product_name AS Model,COUNT(product_name) AS Orders,SUM(quantity) AS TotalProductsSold FROM date_category_count WHERE new_date BETWEEN '2021-07-01' AND '2021-09-30' GROUP BY state,product_category,product_name ORDER BY SUM(quantity) DESC LIMIT 5").show(5)
+q4DFS=spark.sql("SELECT state,product_category AS Brand, product_name AS Model,COUNT(product_name) AS Orders,SUM(quantity) AS TotalProductsSold FROM date_category_count WHERE new_date BETWEEN '2021-10-01' AND '2021-12-31' GROUP BY state,product_category,product_name ORDER BY SUM(quantity) DESC LIMIT 5").show(5)
 
 ####JUSTINS#######
-
+print("Justin's")
 #What times have the highest traffic of sales
 
 #split string step
@@ -158,20 +167,25 @@ formatted_time_df = id_time_df.withColumn("timestamp",  date_format(col("time"),
 # formatted_time_df.printSchema()
 # formatted_time_df.show()
 
+
 formatted_time_df.groupBy(hour("timestamp").alias("hour"))\
     .agg(count("order_id").alias('order_traffic'))\
     .sort('hour')\
-    .show()    
+    .show(5)    
     
 ######What states with the highest traffic of sales
-df.createOrReplaceTempView("table")
-
-highest_traffic_state = spark.sql(" select state , count(state) as qty  from table where state IN ('South Carolina','Mississippi','Virginia','West Virginia','kentucky','Alabama','North Carolina','Arkansas','Louisiana','Tennessee','Florida','Georgia','Hawaii') group by state order by qty desc")
-highest_traffic_state.show()
+print("AdeTunji's")
+highest_traffic_state = spark.sql(" select state , count(state) as qty  from data group by state order by qty desc")
+highest_traffic_state.show(5)
 
 ######What cities with the highest traffic of sales
-highest_traffic_city = spark.sql("select city, count(city) as qty from table group by city order by qty desc ")
-highest_traffic_city.show()
+highest_traffic_city = spark.sql("select city, count(city) as qty from data group by city order by qty desc ")
+highest_traffic_city.show(5)
 
 """highest_traffic_state.coalesce(1).write.csv("file:/USER/output_states")
 highest_traffic_city.coalesce(1).write.csv("file:/USER/output_city")"""
+
+#########Which locations have the highest sales?#####
+print('Nilesh:')
+location_sales = spark.sql("SELECT city, SUM(quantity*price) AS sales FROM data WHERE payment_txn_success = 'Y' GROUP BY city ORDER BY sales DESC")
+location_sales.show(5)
